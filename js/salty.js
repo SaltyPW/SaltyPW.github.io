ecb0382f3beef852723f6e163dd5e567de8497f2
karl <>
2021-12-29 18:34:54 +0200

    Got rid of third-party libraries and switched to native SubtleCrypto and BigInt APIs. All the JS code fits into a single screen now and is trivially verifiable.

--- js/salty.js
+++ js/salty.js
@@ -1,26 +1,38 @@
-/*
-var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var digits = '0123456789';
-var hexDigits = '0123456789abcdef';
-var specials = '~!@#$%^&*()_+=-`[]{};\'\\:"|,./<>?"';
-var allChars = lowercase+uppercase+digits+specials;
-*/
-
-function doHash(pass){
-    var h = sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(pass));
-    console.log(h);
-    var bi = str2bigInt(h.substring(0,16), 16);
-    return bigInt2str(bi, 95);
+function a2BI(buf) {
+    var res = BigInt(0);
+    var base = BigInt(1);
+    for (var i in buf){
+	res += BigInt(buf[buf.length - i - 1])*base;
+	base = base*BigInt(256);
+    }
+    return res;
 }
 
+const digitsStr='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_=!@#$%^&*()[]{}|;:,.<>/?`~ \\\'\"+-';
+
+function base95(n){
+    const b95=BigInt(95);
+    if (n == 0)
+	return '0';
+    var result = ''
+    while (n){
+        result = digitsStr[n % b95] + result;
+        n = n/b95;
+    }
+    return result;
+}
+
+const enc = new TextEncoder();
+
 function makePass(){
-    document.getElementById('input-generated-password').value =  doHash(document.forms['passform']['base'].value +document.forms['passform']['salt'].value);
+    const src = document.forms['passform']['base'].value +document.forms['passform']['salt'].value;
+    crypto.subtle.digest('SHA-256', enc.encode(src)).then(
+	dig => {
+	    document.getElementById('input-generated-password').value = base95(a2BI(new Uint8Array(dig,0, 8)));
+	});
 }
 
 window.onload = function(){
-  document.forms['passform']['base'].focus();
-  document.forms['passform'].onkeypress = function(event){
-    setTimeout(makePass);
-  };  
+    document.forms['passform']['base'].focus();
+    document.forms['passform'].onkeyup = makePass;   
 };
