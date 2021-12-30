f07268be95d17cefb46bbf11001e2db40f1ded10
karl <>
2021-12-30 11:56:47 +0200

    Fixed error in Safari on IOS

--- js/salty.js
+++ js/salty.js
@@ -1,38 +1,40 @@
-function a2BI(buf) {
-    var res = BigInt(0);
-    var base = BigInt(1);
-    for (var i in buf){
-	res += BigInt(buf[buf.length - i - 1])*base;
-	base = base*BigInt(256);
+(function(){
+    function a2BI(buf) {
+	var res = BigInt(0);
+	var base = BigInt(1);
+	for (var i in buf){
+	    res += BigInt(buf[buf.length - i - 1])*base;
+	    base = base*BigInt(256);
+	}
+	return res;
     }
-    return res;
-}
 
-const digitsStr='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_=!@#$%^&*()[]{}|;:,.<>/?`~ \\\'\"+-';
+    const digitsStr='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_=!@#$%^&*()[]{}|;:,.<>/?`~ \\\'\"+-';
 
-function base95(n){
-    const b95=BigInt(95);
-    if (n == 0)
-	return '0';
-    var result = ''
-    while (n){
-        result = digitsStr[n % b95] + result;
-        n = n/b95;
+    function base95(n){
+	const b95=BigInt(95);
+	if (n == 0)
+	    return '0';
+	var result = ''
+	while (n){
+            result = digitsStr[n % b95] + result;
+            n = n/b95;
+	}
+	return result;
     }
-    return result;
-}
 
-const enc = new TextEncoder();
+    const enc = new TextEncoder();
 
-function makePass(){
-    const src = document.forms['passform']['base'].value +document.forms['passform']['salt'].value;
-    crypto.subtle.digest('SHA-256', enc.encode(src)).then(
-	dig => {
-	    document.getElementById('input-generated-password').value = base95(a2BI(new Uint8Array(dig,0, 8)));
-	});
-}
+    function makePass(){
+	const src = document.forms['passform']['base'].value +document.forms['passform']['salt'].value;
+	crypto.subtle.digest('SHA-256', enc.encode(src)).then(
+	    dig => {
+		document.getElementById('input-generated-password').value = base95(a2BI(new Uint8Array(dig,0, 8)));
+	    });
+    }
 
-window.onload = function(){
-    document.forms['passform']['base'].focus();
-    document.forms['passform'].onkeyup = makePass;   
-};
+    window.onload = function(){
+	document.forms['passform']['base'].focus();
+	document.forms['passform'].onkeyup = makePass;   
+    };
+})();
